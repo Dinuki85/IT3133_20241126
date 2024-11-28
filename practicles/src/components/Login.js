91f8a47e8f856eb10a84968f1e25b430029738e9
Dinuki85 <139613301+Dinuki85@users.noreply.github.com>
2024-11-28 18:46:06 +0000

    Login file edited

---
+++ practicles/src/components/Login.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login(){
+    return(
+        <div>
+            <h1>Login Here</h1>
+            <table>
+                <tr>
+                    <td>User Name: </td>
+                    <td><input type="text" name="username" onChange={getTextInput}/> </td>
+                </tr>
+                <tr>
+                    <td>Password: </td>
+                    <td><input type="password" name="pwd" onChange={getTextInput}/> </td>
+                </tr>
+                <tr>
+                    <td style={{ color:'red' }} colspan={2}>{user.error}</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
