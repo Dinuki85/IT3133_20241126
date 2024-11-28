ff133bc0ea08bcd7867c327f30610567735b9417
Dinuki85 <139613301+Dinuki85@users.noreply.github.com>
2024-11-28 18:52:50 +0000

    Login.js File updated

--- practicles/src/components/Login.js
+++ practicles/src/components/Login.js
@@ -2,6 +2,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login(){
+    const navigate = useNavigate();
+    const [user, setUser] = useState({ username: "" , pwd: "",error: ""})
+    const getTextInput = (e)=>{
+        const {name,value} = e.target;
+        setUser((prevUser))=>({
+            ...prevUser,[name]:value,
+        })
+    }
     return(
         <div>
             <h1>Login Here</h1>
